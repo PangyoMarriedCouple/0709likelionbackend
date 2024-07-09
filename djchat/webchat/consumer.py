01004be905dc9fb6b91bc6749ca9e632e260cddd
2017-19620 <ksw2266@snu.ac.kr>
2024-07-09 22:09:39 +0900

    first

--- djchat/webchat/consumer.py
+++ djchat/webchat/consumer.py
@@ -17,9 +17,7 @@ class WebChatConsumer(JsonWebsocketConsumer):
         self.accept()
 
         self.channel_id = self.scope["url_route"]["kwargs"]["channelId"]
-
         self.user = User.objects.get(id=1)
-
         async_to_sync(self.channel_layer.group_add)(self.channel_id, self.channel_name)
 
     def receive_json(self, content):
