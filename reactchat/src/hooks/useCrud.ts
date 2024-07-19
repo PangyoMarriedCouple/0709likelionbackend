12b5a89681ea1f81efa203f88c8ab5973eb02160
2017-19620 <ksw2266@snu.ac.kr>
2024-07-19 17:24:17 +0900

    allowd host added

--- reactchat/src/hooks/useCrud.ts
+++ reactchat/src/hooks/useCrud.ts
@@ -15,16 +15,17 @@ const useCrud = <T>(initalData: T[], apiURL: string): IuseCrud<T> => {
     const [error, setError] = useState<Error | null>(null)
     const [isLoading, setIsLoading] = useState(false)
 
-    const fetchData = async () =>{
+    const fetchData = async () => {
         setIsLoading(true)
-        try{
+        try {
+            console.log(`${BASE_URL}에 연결해보겠습니다...`)
             const response = await jwtAxios.get(`${BASE_URL}${apiURL}`, {})
             const data = response.data
             setDataCRUD(data)
             setError(null)
             setIsLoading(false)
             return data;
-        } catch (error: any){
+        } catch (error: any) {
             if (error.response && error.response.status === 400) {
                 setError(new Error("400"))
             }
@@ -33,6 +34,6 @@ const useCrud = <T>(initalData: T[], apiURL: string): IuseCrud<T> => {
         }
     };
 
-    return {fetchData, dataCRUD, error, isLoading}
+    return { fetchData, dataCRUD, error, isLoading }
 }
 export default useCrud
